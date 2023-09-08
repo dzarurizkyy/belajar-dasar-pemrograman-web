46d12058bd8f538dcf4055554892dd0633ecae39
dzarurizkyy <dzarurizkybusiness@gmail.com>
2023-09-08 19:59:18 +0700

    Added menus section

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 // Variable
 const navbarIconList = document.getElementById("navbar-icon-list");
 const navbarListContent = document.getElementById("navbar-list-content");
+const openAnimation = document.querySelectorAll(".open-animation");
 
 // Functions
 navbarIconList.addEventListener("click", () => {
   navbarListContent.classList.toggle("hidden");
 });
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+openAnimation.forEach((el) => observer.observe(el));
